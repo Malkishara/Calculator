e31d3abeaf2593ccaa5adee97e895ceb5957eece
Malkishara <isurumalkishara97@gmail.com>
2021-08-07 22:39:36 +0530

    initial commit

--- src/java/com/kelaniya/uni/Main.java
+++ src/java/com/kelaniya/uni/Main.java
@@ -1,2 +1,44 @@
-package com.kelaniya.uni;public class Main {
+package com.kelaniya.uni;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static int a;
+    public static int b;
+    public static int i=0;
+    public static void main(String[] args) {
+        try {
+            File f1 = new File("C:/Users/DELL/Desktop/Calculator/src/java/com/kelaniya/uni/Numbers.txt");
+            Scanner scn = new Scanner(f1);
+
+            while (scn.hasNextInt()) {
+                i++;
+
+                switch (i) {
+                    case 1:
+                        a = scn.nextInt();
+                        break;
+                    case 2:
+                        b = scn.nextInt();
+                        break;
+                    default:
+                        System.out.println("There are more than 2 numbers");
+                        break;
+                }
+
+            }
+
+            operations obj = new operations();
+            obj.operation(a, b);
+
+
+
+
+        }catch(FileNotFoundException e){
+            System.out.println("The file did not find.");
+        }
+    }
+
 }
